f92910a0a438d8a2a77582f49c83c70bf078e3e3
tulikakakati <36178396+tulikakakati@users.noreply.github.com>
2020-12-06 13:34:45 +0530

    Add files via upload

--- General learning/DatasetTransform_global_pooled_all.py
+++ General learning/DatasetTransform_global_pooled_all.py
@@ -1,10 +1,6 @@
-#DEGnext
-#Copyright Tulika Kakati 2020
-#Distributed under GPL v3 open source license
-#This code may be used in DEGnext.py main file for transforming the datasets
-##############################################################
-
 from torch.utils.data import Dataset
+from sklearn.preprocessing import StandardScaler
+
 import numpy as np
 import torch
 import math
@@ -14,15 +10,22 @@ class Compose(object):
         self.transforms = transforms
 
     def __call__(self, gene_exp):
+        #print('before',type(gene_exp))
         for transform in self.transforms:
             gene_exp= transform(gene_exp)
+
+
         return gene_exp
 
 class ArrayToTensor(object):
     """Converts a list of numpy.ndarray (H x W x C) along with a intrinsics matrix to
         a list of torch.FloatTensor of shape (C x H x W)."""
     def __call__(self, gene_exp):
+        #a=gene_exp.shape[0]
+        #print(gene_exp.shape[0])
+
         if(is_prime(gene_exp.shape[0])==True):
+            print("Prime number")
         a = gene_exp.shape[0]
         a= math.sqrt(a)
         a=np.floor(a)
@@ -33,6 +36,7 @@ class ArrayToTensor(object):
                     a=a-1
 
         gene_exp_new = np.reshape(gene_exp, (1,-1, a))
+
         gene_exp_new=torch.from_numpy(gene_exp_new).float()
         return gene_exp_new
 
